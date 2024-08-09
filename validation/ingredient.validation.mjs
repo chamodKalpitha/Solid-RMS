4aa1a06b5c44ddcf8e72f0c290f72eab6a3acaa6
chamodKalpitha <chamodlive@live.com>
2024-08-09 19:13:01 +0530

    fix(ingredient validation) add some fields to ingredient update schema

--- validation/ingredient.validation.mjs
+++ validation/ingredient.validation.mjs
@@ -46,6 +46,21 @@ export const ingredientUpdateSchema = Joi.object({
   unit: Joi.string().optional().messages({
     "string.empty": "Unit cannot be empty.",
   }),
+  price: Joi.number().positive().precision(2).optional().messages({
+    "number.base": "The price must be a number.",
+    "number.positive": "The price must be a positive number.",
+    "number.precision": "The price can have at most 2 decimal places.",
+    "any.required": "The price is a required field.",
+  }),
+  url: Joi.string()
+    .uri({
+      scheme: ["http", "https"],
+    })
+    .optional()
+    .messages({
+      "string.uri": "The URL must be a valid HTTP or HTTPS URL.",
+      "any.required": "The URL is a required field.",
+    }),
 }).options({
   abortEarly: false,
 });
