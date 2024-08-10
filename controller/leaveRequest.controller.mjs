ce05618c3ac079d6934be01442dfa6ba714e6f5a
chamodKalpitha <chamodlive@live.com>
2024-08-11 00:05:52 +0530

    feat(leave request) created get all leave request feature and wrote the documentation

--- controller/leaveRequest.controller.mjs
+++ controller/leaveRequest.controller.mjs
@@ -1,11 +1,14 @@
 import prisma from "../prisma/prismaClient.mjs";
-import leaveRequestSchema from "../validation/leaveRequest.validation.mjs";
+import {
+  createLeaveRequestSchema,
+  getAllLeaveRequestSchema,
+} from "../validation/leaveRequest.validation.mjs";
 import "dotenv/config";
 
 export async function addLeaveRequest(req, res) {
   let managerId = req.user.managerId;
   const ownerId = req.user.ownerId;
-  const { error, value } = leaveRequestSchema.validate(req.body);
+  const { error, value } = createLeaveRequestSchema.validate(req.body);
   const { type, from, noOfDate, reason, employeeId } = value;
 
   if (error) {
@@ -62,6 +65,48 @@ export async function addLeaveRequest(req, res) {
   } catch (err) {
     console.error(err);
     return res
+      .status(500)
+      .json({ status: "error", message: ["Internal server error"] });
+  }
+}
+
+export async function getAllLeaveRequest(req, res) {
+  const ownerId = req.user.ownerId;
+
+  try {
+    const { error, value } = getAllLeaveRequestSchema.validate(req.query);
+
+    if (error) {
+      const errorRespond = error.details.map((err) => err.message);
+      return res.status(400).json({ status: "error", message: errorRespond });
+    }
+    const { cursor, take } = value;
+    const takeNumber = parseInt(take) || 10;
+    const cursorObject = cursor ? { id: parseInt(cursor) } : undefined;
+
+    const leaveRequest = await prisma.leaveRequest.findMany({
+      take: takeNumber,
+      skip: cursorObject ? 1 : 0,
+      cursor: cursorObject,
+      where: {
+        ownerId,
+      },
+      include: {
+        employee: true,
+      },
+    });
+
+    const nextCursor =
+      leaveRequest.length === takeNumber
+        ? leaveRequest[leaveRequest.length - 1].id
+        : null;
+
+    res
+      .status(200)
+      .json({ status: "success", data: { leaveRequest, nextCursor } });
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") console.error(error);
+    res
       .status(500)
       .json({ status: "error", message: ["Internal server error"] });
   }
