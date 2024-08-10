97c0f40bd570fc48038ef8f73833649e28f02604
chamodKalpitha <chamodlive@live.com>
2024-08-10 13:48:30 +0530

    feat(employee): Add edit employee feature

--- validation/employee.validation.mjs
+++ validation/employee.validation.mjs
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export default Joi.object({
+export const createEmployeeSchema = Joi.object({
   name: Joi.string().required().messages({
     "string.empty": "Name is required.",
     "any.required": "Name is required.",
@@ -54,4 +54,69 @@ export default Joi.object({
   }),
 }).options({
   abortEarly: false,
+});
+
+export const updateEmployeeSchema = Joi.object({
+  name: Joi.string().optional().messages({
+    "string.empty": "Name is required.",
+    "any.required": "Name is required.",
+  }),
+  nicNo: Joi.string().optional().messages({
+    "string.empty": "NIC number is required.",
+    "any.required": "NIC number is required.",
+  }),
+  address: Joi.string().optional().messages({
+    "string.empty": "Address is required.",
+    "any.required": "Address is required.",
+  }),
+  contactNo: Joi.string().optional().messages({
+    "string.empty": "Contact number is required.",
+    "any.required": "Contact number is required.",
+  }),
+  emgConNo: Joi.string().optional().messages({
+    "string.empty": "Emergency contact number is required.",
+    "any.required": "Emergency contact number is required.",
+  }),
+  emgConName: Joi.string().optional().messages({
+    "string.empty": "Emergency contact name is required.",
+    "any.required": "Emergency contact name is required.",
+  }),
+  designation: Joi.string().optional().messages({
+    "string.empty": "Designation is required.",
+    "any.required": "Designation is required.",
+  }),
+  isCritical: Joi.boolean().optional().messages({
+    "boolean.base": "Is Critical must be a boolean.",
+    "any.required": "Is Critical is required.",
+  }),
+  salary: Joi.number().optional().messages({
+    "number.base": "Salary must be a number.",
+    "any.required": "Salary is required.",
+  }),
+  url: Joi.string()
+    .uri({
+      scheme: ["http", "https"],
+    })
+    .optional()
+    .messages({
+      "string.uri": "The URL must be a valid HTTP or HTTPS URL.",
+      "any.required": "The URL is a required field.",
+    }),
+  outletId: Joi.number().integer().positive().optional().messages({
+    "number.base": "Outlet ID must be a number.",
+    "number.integer": "Outlet ID must be an integer.",
+    "number.positive": "Outlet ID must be a positive number.",
+    "any.required": "Outlet ID is required.",
+  }),
+}).options({
+  abortEarly: false,
+});
+
+export const employeeIdSchema = Joi.object({
+  id: Joi.number().integer().positive().required().messages({
+    "number.base": "Employee ID must be a number.",
+    "number.integer": "Employee ID must be an integer.",
+    "number.positive": "Employee ID must be a positive number.",
+    "any.required": "Employee ID is required.",
+  }),
 });
