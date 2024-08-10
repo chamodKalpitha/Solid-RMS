17d50854698813af80bd475abc01076969c8def0
chamodKalpitha <chamodlive@live.com>
2024-08-10 13:48:30 +0530

    feat(employee): Add edit employee feature

--- controller/employee.controller.mjs
+++ controller/employee.controller.mjs
@@ -1,10 +1,14 @@
 import prisma from "../prisma/prismaClient.mjs";
-import employeeSchema from "../validation/employee.validation.mjs";
+import {
+  createEmployeeSchema,
+  employeeIdSchema,
+  updateEmployeeSchema,
+} from "../validation/employee.validation.mjs";
 import "dotenv/config";
 
 export async function createEmployee(req, res) {
   const ownerId = req.user.ownerId;
-  const { error, value } = employeeSchema.validate(req.body);
+  const { error, value } = createEmployeeSchema.validate(req.body);
   const {
     name,
     nicNo,
@@ -77,3 +81,77 @@ export async function createEmployee(req, res) {
   }
 }
 
+export async function updateEmplyee(req, res) {
+  const ownerId = req.user.ownerId;
+  const { error: idError, value: employeeId } = employeeIdSchema.validate(
+    req.params
+  );
+  const { id } = employeeId;
+  const { error: dataError, value: employeeData } =
+    updateEmployeeSchema.validate(req.body);
+  let errors = [];
+
+  if (idError) {
+    const errorRespond = idError.details.map((err) => err.message);
+    return res.status(400).json({ status: "error", message: errorRespond });
+  }
+
+  if (dataError) {
+    const errorRespond = dataError.details.map((err) => err.message);
+    return res.status(400).json({ status: "error", message: errorRespond });
+  }
+
+  try {
+    // Check for valid Employee
+    const currentEmployee = await prisma.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!currentEmployee) {
+      return res
+        .status(400)
+        .json({ status: "error", message: ["Invalid Employee Id"] });
+    }
+
+    const uniqueCheck = await prisma.employee.findFirst({
+      where: {
+        OR: [{ nicNo: employeeData.nicNo }],
+      },
+    });
+
+    if (uniqueCheck) {
+      if (uniqueCheck.nicNo === employeeData.nicNo) {
+        errors.push("Employee with same NIC already exists");
+      }
+    }
+
+    if (employeeData.outletId) {
+      const existingOutlet = await prisma.outlet.findUnique({
+        where: { id: employeeData.outletId },
+      });
+      if (!existingOutlet) errors.push("Invalid Outlet Id");
+      if (existingOutlet && existingOutlet.ownerId !== ownerId)
+        errors.push("Invalid Outlet Id");
+    }
+
+    if (errors.length > 0) {
+      return res.status(400).json({ status: "error", message: errors });
+    }
+
+    const updatedEmployee = await prisma.employee.update({
+      where: {
+        id,
+      },
+      data: employeeData,
+    });
+
+    res.status(200).json({ status: "success", data: updatedEmployee });
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") console.error(error);
+    res
+      .status(500)
+      .json({ status: "error", message: ["Internal server error"] });
+  }
+}
--- route/employee.route.mjs
+++ route/employee.route.mjs
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createEmployee } from "../controller/employee.controller.mjs";
+import {
+  createEmployee,
+  updateEmplyee,
+} from "../controller/employee.controller.mjs";
 import checkRole from "../middleware/authorizationChecker.middleware.mjs";
 
 const router = Router();
 
 router.post("/new", checkRole("OWNER"), createEmployee);
+router.patch("/edit/:id", checkRole("OWNER"), updateEmplyee);
 
 export default router;
 
