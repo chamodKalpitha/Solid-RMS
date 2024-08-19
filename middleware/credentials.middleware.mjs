e71813a75d4963eef242be2e9f6089e74a2814ff
W A Chamod Kalpitha <151531645+chamodKalpitha@users.noreply.github.com>
2024-08-19 08:20:10 +0530

    Delete middleware/credentials.middleware.mjs

--- middleware/credentials.middleware.mjs
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = ["http://localhost:3500"];
-
-export default function credentials(req, res, next) {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
-  next();
-}
